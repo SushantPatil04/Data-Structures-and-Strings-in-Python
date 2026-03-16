7be493815fad64d1bfeb46c8d32ac97302767df1
SushantPatil04 <patilsushant9767@gmail.com>
2026-03-16 20:03:05 +0530

    Add files via upload
    Demonstrate List Slicing  completed task 2

---
+++ task2.py
@@ -0,0 +1,11 @@
+list = list(range(1,11))
+
+five = list[0:5]
+
+reverse =five[::-1]
+
+print("original list", list)
+print("Extracted first five element :", five)
+print("Reversed Extracted element :", reverse)
+
+
